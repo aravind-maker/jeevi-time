acd6c35de5853a37617802514906f4c28cc0db9f
jeevitha-jevi <jeevitha@fixnix.co>
2019-03-04 13:37:45 +0530

    new

--- addclient.php
+++ addclient.php
@@ -1,29 +1,15 @@
+
 <!DOCTYPE html>
 <?php
 require "header.php";
 ?>
 <html>
 <head>
-  <title></title>
+  <title>first edit</title>
+
   <style>
 
   
-
-.topnav {
-  /*overflow: hidden;*/
-  background-color: #f1f1f1;
-}
-
-.topnav a {
-  float: left;
-  display: block;
-  color: black;
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-  border-bottom: 3px solid transparent;
-}
 
 .topnav a:hover {
   border-bottom: 3px solid #00C4D0;
